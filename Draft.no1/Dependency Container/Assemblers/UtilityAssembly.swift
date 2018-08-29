0486aad22ce9ffccf0146947bd072cf9991e8f2d
Daniel Budynski <daniel@prismake.com>
2018-08-30 01:38:04 +0200

    DependencyContainer with Swinject

---
+++ Draft.no1/Dependency Container/Assemblers/UtilityAssembly.swift
@@ -0,0 +1,14 @@
+// Draft.no1
+// Created by Daniel Budyński 👾 on 30.08.2018
+// MacOS 10.13, Swift 4.0
+// Copyright © 2018 Budyn And Friends. All rights reserved.
+
+import Swinject
+
+class UtilityAssembly: Assembly {
+
+    func assemble(container: Container) {
+
+    }
+
+}
---
+++ Draft.no1/Dependency Container/DependencyContainer.swift
@@ -0,0 +1,21 @@
+// Draft.no1
+// Created by Daniel Budyński 👾 on 30.08.2018
+// MacOS 10.13, Swift 4.0
+// Copyright © 2018 Budyn And Friends. All rights reserved.
+
+import Swinject
+
+class DependencyContainer {
+
+    private lazy var assembler: Assembler = {
+        return Assembler([
+            ViewModelAssembly(),
+            ViewControllerAssembly(),
+            FlowControllerAssembly(),
+            BuilderAssembly(),
+            ServiceAssembly(),
+            UtilityAssembly()
+        ])
+    }()
+
+}
