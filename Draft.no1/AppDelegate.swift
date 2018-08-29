b46dbb865313e679c5d80a3c19b2251652f71ac3
Daniel Budynski <daniel@prismake.com>
2018-08-30 01:09:13 +0200

    Carthage support

---
+++ Draft.no1/AppDelegate.swift
@@ -0,0 +1,31 @@
+// Draft.no1
+// Created by Daniel Budyński 👾 on 30.08.2018
+// MacOS 10.13, Swift 4.0
+// Copyright © 2018 Budyn And Friends. All rights reserved.
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+
+        
+
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {}
+
+    func applicationDidEnterBackground(_ application: UIApplication) {}
+
+    func applicationWillEnterForeground(_ application: UIApplication) {}
+
+    func applicationDidBecomeActive(_ application: UIApplication) {}
+
+    func applicationWillTerminate(_ application: UIApplication) {}
+
+}
+
